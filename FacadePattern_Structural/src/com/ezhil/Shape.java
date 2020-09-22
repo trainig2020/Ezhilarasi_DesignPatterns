ef2effb82cb96b46bad6415cf3cd203a035dac7f
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-09-22 19:44:19 +0530

    Add files via upload

---
+++ FacadePattern_Structural/src/com/ezhil/Shape.java
@@ -0,0 +1,6 @@
+package com.ezhil;
+
+public interface Shape {
+	void draw();
+
+}
---
+++ FacadePattern_Structural/src/com/ezhil/Square.java
@@ -0,0 +1,9 @@
+package com.ezhil;
+
+public class Square implements Shape {
+
+	   @Override
+	   public void draw() {
+	      System.out.println("Square::draw()");
+	   }
+	}
